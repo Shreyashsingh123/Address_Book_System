1aea5adbd5dfc897f76e93f9b18178bac5cec680
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-03-13 12:18:44 +0530

    Modified Address_Book_main.py, Refactor to add multiple Address book to the system and Address book having unique name

--- Address_book_main.py
+++ Address_book_main.py
@@ -1,12 +1,14 @@
 from Contact import Contact
 from Address_Book import Address_Book
-
+from Address_Book_Manager import Address_Book_Manager
 # taken multiple user input and add it to contact class
-book1=Address_Book()
-
+books=Address_Book_Manager()
 takeinput=True
-while(takeinput):
-    
+while(takeinput):   
+   n=input("Enter  Address book name: ")
+   books.add_Address_Book_to_dict(n)
+   book1=books.Address_Book_dict[n]
+
    F_name=input("Enter first name:")
    L_name=input("Enter Last name:")
    address=input("Enter your Address:")
@@ -30,10 +32,10 @@ while(takeinput):
    book1.add_Contact(cont1)
    print("Contact added successfully!\n")
 
-   print(" Choose 1 to Display Contacts")
-   print(" Choose 2 to edit Contact")
-   print(" Choose 3 to Delete Contact")
-   print(" Choose 0 to Exit")
+   print("Choose 1 to Display Contacts")
+   print("Choose 2 to edit Contact")
+   print("Choose 3 to Delete Contact")
+   print("Choose 0 to Exit")
    choice = int(input("Enter your choice: "))
    match choice:
         case 0:
@@ -53,17 +55,8 @@ while(takeinput):
    addperson = input("Add another person? (y/n): ")
    if addperson.lower() != 'y':
       takeinput=False
-   
-   
-# e1=input("If you want to e1 details choose y or choose n [y/n]")
-# if e1.upper()=='Y':
-#    #  if yes then edit existing contact person using their name or first name
-#     n=input("Enter the first name of user whose detail you want to edit: ")
-#     book1.editdetails(n)
-# book1.display_contact()
 
-# # enter user to delete 
-# deleteuser=input("Choose y to delete contact and n to not [Y/N]")
-# if deleteuser.upper()=='Y':
-#     n=input("Enter first name of user whose details you want to delete")
-#     book1.deleteDetails(n)
+d=input("Do you want to display Adress Book or not [Y/N]")
+if d.lower()=='y':
+    books.display_Address_Book()
+   
