175f443e36b570140108cacd2cce04f754f2002f
Shreyashsingh123 <singhshreyash631@gmail.com>
2026-03-13 12:49:22 +0530

    Added Address_Book_Manager.py , Modified code to store unique names

--- Address_Book_Manager.py
+++ Address_Book_Manager.py
@@ -3,6 +3,7 @@ from Address_Book import Address_Book
 class Address_Book_Manager: #to manage mutlitple contacts
     def __init__(self):
         self.Address_Book_dict={}  # craeted dictinary to store multiple address book
+        
     def add_Address_Book_to_dict(self,name):
         self.Address_Book_dict[name]=Address_Book()
     def display_Address_Book(self):
